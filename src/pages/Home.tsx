4ac21bf181824dfda73c9ec9b3cd53d5a2d3a419
Mateus Hanauer Roerig <mateus.roerig@hotmail.com>
2021-08-28 23:44:41 -0300

    feat: implementations

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,31 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(), 
+      title: newTaskTitle, 
+      done: false
+    }
+    
+    setTasks(state => [...state, data])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        task.done = !task.done
+      }
+      
+      return task
+    })
+
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTasks = tasks.filter(task => task.id !== id)
+
+    setTasks(updatedTasks)
   }
 
   return (
